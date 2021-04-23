1aa11ff5a9271ae5200dd4fe8772fd213b7e58fc
Titouan-goulhot <titouan.goulhot-bonichon@le-campus-numerique.fr>
2021-04-23 15:04:40 +0200

    ajout fichier .gitignore

--- support-projet-4-master/assets/js/script.js
+++ support-projet-4-master/assets/js/script.js
@@ -8,5 +8,41 @@ var typed = new Typed('.typed',{
     loop: true
 });
 
+// ********************************************COMPTEUR*******************************
 
+// let compteur = 0;
 
+// $(window).scroll(function() {
+
+//     const top = $('.counter').offset().top -
+//      window.innerHeight;
+
+//     //si le compteur  est à 0 et qu'on scroll ....
+//     if(compteur == 0 && $(window).scrollTop() > top){
+//         $('.counter-value').each(function() {
+//             let $this = $(this),
+//             countTO = $this.attr('data-count');
+//             $({
+//                 countNum: $this.text()
+//             }).animate({
+//                 countNum : countTO
+//             },
+//             {
+//                 duration: 10000,
+//                 easing: 'swing',
+//                 step: function() {
+//                     $this.text(Math.floor(this.countNum));
+//             },
+//             complete: function(){
+//                 $this.text(this.countNum);
+//             }
+//             });
+//         });
+//         // Pour ne pas relancer la fonction à chaque fois
+//         compteur = 1;
+//     }
+// });
+
+//AOS
+
+AOS.init();
