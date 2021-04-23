bff8f5d1a97608a56ffdf4af286d45698ec1f93a
Titouan-goulhot <titouan.goulhot-bonichon@le-campus-numerique.fr>
2021-04-23 14:59:02 +0200

    Test commit

---
+++ support-projet-4-master/assets/js/script.js
@@ -0,0 +1,12 @@
+
+//TYPED
+var typed = new Typed('.typed',{
+    strings: [' Mon parcours',' Mes compétences', ' Mes envies', ' Mes passions' ],
+    typeSpeed: 20,
+    backSpeed : 0,
+    smartBackspace: true,
+    loop: true
+});
+
+
+
